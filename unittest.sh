fbe6e2be76862ce7932ca7b976c8669418cd5a35
jinhyuck <jinhyuck83.park@samsung.com>
2018-02-07 08:28:59 +0900

    modify unittest.sh for CI build

--- unittest.sh
+++ unittest.sh
@@ -2,29 +2,12 @@
 
 export GOPATH=$PWD
 
-go get github.com/golang/mock/gomock
-go get github.com/ghodss/yaml
-go get -d docker.io/go-docker
-go get -d github.com/docker/libcompose
-go get golang.org/x/sys/unix
-go get github.com/shirou/gopsutil
-
 rm -rf $GOPATH/src/github.com/docker/distribution/vendor/github.com/opencontainers
 
-pkg_list=("api/common" "api/health" "api/monitoring/resource" "controller/deployment/dockercontroller" "controller/health" "controller/monitoring/resource" "controller/shellcommand" "db/mongo/service" "messenger")
-
-function func_cleanup(){
-    rm *.out *.test
-    rm -rf $GOPATH/pkg
-    rm -rf $GOPATH/src/docker.io
-    rm -rf $GOPATH/src/golang.org
-    rm -rf $GOPATH/src/github.com
-}
+pkg_list=()
 
 count=0
 for pkg in "${pkg_list[@]}"; do
- go test -c -v -gcflags "-N -l" $pkg
- go test -coverprofile=$count.cover.out $pkg
  if [ $? -ne 0 ]; then
     echo "Unittest is failed."
     func_cleanup
@@ -33,10 +16,6 @@ for pkg in "${pkg_list[@]}"; do
  count=$count.0
 done
 
-echo "mode: set" > coverage.out && cat *.cover.out | grep -v mode: | sort -r | \
-awk '{if($1 != last) {print $0;last=$1}}' >> coverage.out
-
 go tool cover -func=coverage.out
 go tool cover -html=coverage.out -o coverall.html
 
-func_cleanup
