2bb6358b5c5388ca17f9ff88487ffe4bf05573b2
jinhyuck <jinhyuck83.park@samsung.com>
2018-04-04 14:40:35 +0900

    fix build error

--- src/main/main.go
+++ src/main/main.go
@@ -28,7 +28,6 @@ import (
 	. "config"
 	. "tns_model"
 	. "db/mongo"
-	. "rest"
 )
 
 var config = Config{}
@@ -58,137 +57,11 @@ func TriggerHealthCheck(){
 }
 
 
-// Discover topic
-// GET list of tns topics including keyword check
-func AllTNSServerList(w http.ResponseWriter, r *http.Request) {
-	queryValues := r.URL.Query()
-	mytopic := queryValues.Get("topic")						 
-	fmt.Printf("query : %v\n",queryValues)							 
-	fmt.Printf("topic : %s\n",mytopic)							 
-	tnsdata, err := tns.DiscoverTopic(mytopic)
-//	tnsdata, err := tns.FindAll()
-		if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	respondWithJson(w, http.StatusOK, tnsdata)
-}
-
-// Resolution list of tns topics :: PUT => need to change to GET
-func ResolutionTopic_PUT(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var tnsdata TNSdata
-	if err := json.NewDecoder(r.Body).Decode(&tnsdata); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-			return
-	}
-	tnsdata.ID = bson.NewObjectId()
-	mytopic := tnsdata.Topic	
-	tnsdata_res, err := tns.DiscoverTopic(mytopic)	
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-			return
-	}
-	respondWithJson(w, http.StatusOK, tnsdata_res)
-}
-
-// Discover topic by keyword
-// GET/{topic}
-func DiscoverByTopic(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	tnsdata_res, err := tns.DiscoverTopic(params["topic"])	
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid topic")
-			return
-	}
-	respondWithJson(w, http.StatusOK, tnsdata_res)
-}
-
-// Discover topic by id
-// GET/{id}
-func FindTNSList(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	tnsdata, err := tns.FindById(params["id"])
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid id")
-		return
-	}
-	// TO DO error check for all cases				
-	respondWithJson(w, http.StatusOK, tnsdata)
-}
-
-// Register Topic
-// POST a new list
-func CreateTopicList(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var tnsdata TNSdata
-	if err := json.NewDecoder(r.Body).Decode(&tnsdata); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	// Validation CHECK for duplicate TOPIC
-	if err := tns.CheckDuplicate(tnsdata); err != false {
-//		respondWithError(w, http.StatusBadRequest, "Duplicated Topic")
-    	respondWithJson(w, http.StatusOK, map[string]string{"result": "duplicated"})
-		return
-	}
-	println("Now will be Updated")
-	tnsdata.ID = bson.NewObjectId()
-	if err := tns.Insert(tnsdata); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	//respondWithJson(w, http.StatusCreated, tnsdata)
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
-}
-
-// PUT update an existing lists
-func UpdateTopicList(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var tnsdata TNSdata
-	if err := json.NewDecoder(r.Body).Decode(&tnsdata); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	if err := tns.Update(tnsdata); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
-}
-
-// Unregister Topic
-// DELETE an existing lists
-func DeleteTNSList(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var tnsdata TNSdata
-	if err := json.NewDecoder(r.Body).Decode(&tnsdata); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	if err := tns.Delete(tnsdata); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
-}
-
 // POST healthcheck for Topics in TNS server
 func TopicHealthcheck(w http.ResponseWriter, r *http.Request) {
 // TODO
 // GET topic and check for existing TNSDB
 // after all check for TNSDB, if there is unchecked topic, than delete it 	
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	respondWithJson(w, code, map[string]string{"error": msg})
-}
-
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
 }
 
 // Parse the configuration file 'config.toml', and establish a connection to DB
